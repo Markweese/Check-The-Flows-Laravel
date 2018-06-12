0ff028826cc415696772b5bd835f33b6f6986fa4
Mark Brewer <mark@emersonstone.com>
2018-06-12 12:23:09 -0600

    data formatted into associative array

--- app/Jobs/SyncHistoricReadings.php
+++ app/Jobs/SyncHistoricReadings.php
@@ -50,26 +50,81 @@ class SyncHistoricReadings implements ShouldQueue
       }
 
       foreach($stations as $station) {
+        //goes to the site selection page and scrapes for ts_id and site_id
         $htmlUrl = 'https://waterdata.usgs.gov/nwis/monthly/?format=sites_selection_links&search_site_no=' . $station->usgs_id . '&amp;referred_module=sw';
+        $ts_id = null;
+        $site_id = null;
+        $stats_url = null;
 
+        //domdocument grabs the html, and only parses out input elements
         $dom = new \DOMDocument;
-        //   $res = $client->request('GET', $url);
-        //   $html = $res->getBody()->getContents();
-
         @$dom->loadHTMLFile($htmlUrl);
-
         $inputs = $dom->getElementsByTagName('input');
 
-        foreach ( $inputs as $input) {
-            $name = $input->getAttribute('name');
-            $alt = $input->getAttribute('alt');
-            if($name == 'ts_id_' . $station->usgs_id) {
-                echo $input->getAttribute('value');
-                return;
+        foreach ( $inputs as $i => $value) {
+            //name grabs inputs with a matching name attr
+            //paramCheck makes sure it is a 00060 measurement
+            //*paramCheck will break if the html page structure changes
+            $name = $inputs[$i]->getAttribute('name');
+            $paramCheck = null;
+
+            //make sure the following element exists, and grab it's parameter id
+            if($inputs[$i+1] != null) {
+                $paramCheck = $inputs[$i+1]->getAttribute('value');
+            }
+
+            //grab the ts_id element, and make sure the following param is cfs
+            if($name == 'ts_id_' . $station->usgs_id && $paramCheck == '00060') {
+                $ts_id = $inputs[$i]->getAttribute('value');
+            }
+
+            //grab site_id. 
+            //This value stays consistent throughout the page, unlike ts_id
+            if($name == 'site_id_' . $station->usgs_id) {
+                $site_id = $inputs[$i]->getAttribute('value');
+            }
+
+            //break the loop once we've got our keys
+            if(!empty($ts_id) && !empty($site_id)) {
+                break;
             }
         }
+
+        //let the program rest 1 second
+        //Start grabbing tab delmimited data
           
-        usleep(200000);
+        usleep(1000000);
+        
+        //url for rdb file
+        $stats_url =  'https://waterdata.usgs.gov/nwis/monthly?site_no=' . $station->usgs_id  . '&agency_cd=USGS&por_' . $station->usgs_id  . '_' . $ts_id  . '=' . $site_id  . ',00060,' . $ts_id  . ',1930-01,2018-06&referred_module=sw&format=rdb';
+
+        //gets and stores the tab delimited file
+        $res = $client->request('GET', $stats_url);
+        $rdbString = $res->getBody()->getContents();
+
+        $rdbRows = explode(PHP_EOL, $rdbString);
+        $historicStats = array();
+
+        foreach ($rdbRows as $row) {
+            $stats = str_getcsv($row, "\t");
+
+            if(count($stats) == 7 && ctype_digit($stats[5])) {
+                $historicStats[$stats[5]][] = $stats[6];
+            }
+        }
+
+        foreach($historicStats as $stat) {
+            print_r($stat[1]);
+        }
+
+        //run stats on the array
+        //print_r($historicStats);
+        // foreach($historicStats as $stat) {
+
+        //     if(count($stat) == 7 && ctype_digit($stat[6])) {
+        //         var_dump($stat[6]);
+        //     }
+        // }
       }
 
       return;
